a11f22c29bfef88fb31b52f86d3ad3cd1805b903
vegirevathi <vegirevathi@gmail.com>
2020-08-25 14:57:42 +0530

    ADDED: Random function to check employee status

--- empWageReview2.cpp
+++ empWageReview2.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 int main() {
 	cout << "Welcome to Employee Wage Computation " << endl;
+
+	int empStatus;
+	srand(time(NULL));
+	empStatus = rand() % 2;
+	if (empStatus == 1) {
+		cout << "employee is Present " << endl;
+	}
+	else {
+		cout << "Employee is Absent " << endl;
+	}
 
 	return 0;
 }
