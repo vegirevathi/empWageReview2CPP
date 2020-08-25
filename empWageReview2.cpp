b9e97e9c04c0f0ba323d53e1c0b2d524cf01f87e
vegirevathi <vegirevathi@gmail.com>
2020-08-25 15:47:06 +0530

    ADDED: Part time employee and wage

--- empWageReview2.cpp
+++ empWageReview2.cpp
@@ -2,23 +2,38 @@
 #include <ctime>
 using namespace std;
 
-void empWage() {
-	int empStatus;
-	const int WAGE_PER_HOUR = 20;
+int getWorkingHrs() {
 	int empHrs;
    srand(time(NULL));
-   empStatus = rand() % 2;
-   if (empStatus == 1) {
-      cout << "employee is Present " << endl;
+
+   int empStatus = rand() % 3;
+
+	const int IS_FULL_TIME = 1;
+	const int IS_PART_TIME = 2;
+
+	switch (empStatus)
+	{
+	case IS_FULL_TIME:
+      cout << " Employee is Full Time Present " << endl;
 		empHrs = 8;
-   }
-   else {
+		break;
+   case IS_PART_TIME:
+		cout << "Employee is Part Time Present " << endl;
+		empHrs = 4;
+		break;
+	default:
       cout << "Employee is Absent " << endl;
 		empHrs = 0;
+		break;
    }
 
-	int dailyWage = empHrs * WAGE_PER_HOUR;
-	cout << "Daily wage is " << dailyWage;
+	return empHrs;
+}
+
+void empWage() {
+	int WAGE_PER_HOUR = 20;
+	int dailyWage = getWorkingHrs() * WAGE_PER_HOUR;
+   cout << "Daily wage is " << dailyWage;
 }
 
 
