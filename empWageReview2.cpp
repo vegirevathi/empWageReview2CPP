85dd8a4acaa6ce3959a5d69b35dca7f3ffeef5f0
vegirevathi <vegirevathi@gmail.com>
2020-08-25 15:11:37 +0530

    ADDED: Method to calculate daily employee wage

--- empWageReview2.cpp
+++ empWageReview2.cpp
@@ -2,18 +2,29 @@
 #include <ctime>
 using namespace std;
 
+void empWage() {
+	int empStatus;
+	const int WAGE_PER_HOUR = 20;
+	int empHrs;
+   srand(time(NULL));
+   empStatus = rand() % 2;
+   if (empStatus == 1) {
+      cout << "employee is Present " << endl;
+		empHrs = 8;
+   }
+   else {
+      cout << "Employee is Absent " << endl;
+		empHrs = 0;
+   }
+
+	int dailyWage = empHrs * WAGE_PER_HOUR;
+	cout << "Daily wage is " << dailyWage;
+}
+
+
 int main() {
 	cout << "Welcome to Employee Wage Computation " << endl;
 
-	int empStatus;
-	srand(time(NULL));
-	empStatus = rand() % 2;
-	if (empStatus == 1) {
-		cout << "employee is Present " << endl;
-	}
-	else {
-		cout << "Employee is Absent " << endl;
-	}
-
+	empWage();
 	return 0;
 }
